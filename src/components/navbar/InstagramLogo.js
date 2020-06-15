84ca75c4fef55dd31b7014e35efcf2b34a7c3dfe
nwa24 <nwa24@sfu.ca>
2020-06-14 18:57:45 -0700

    NavigationBar: Updated Instagram account linked to icon

--- src/components/navbar/InstagramLogo.js
+++ src/components/navbar/InstagramLogo.js
@@ -3,7 +3,10 @@ import InstagramIcon from "../../images/instagram-logo.png"
 
 const InstagramLogo = () => {
   return (
-    <a href="https://www.instagram.com/kailey.tam/" className="instagramLogo">
+    <a
+      href="https://www.instagram.com/wordsbykailey/"
+      className="instagramLogo"
+    >
       <img src={InstagramIcon} alt="logo" />
     </a>
   )
