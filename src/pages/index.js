a6cf191d58397795574dc67bf20cfb23454ab293
nwa24 <nwa24@sfu.ca>
2020-07-12 14:39:56 -0700

    43: Update the text in the "Coming Soon" section

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,15 +53,16 @@ const IndexPage = props => {
               color: "#000000",
             }}
           >
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            ON THE WAY HOME is a handful of poems written over the course of
+            five years about growing up. The narrative begins with baby steps
+            away from home, escalates with the conflict of taking a wrong turn,
+            accepts the need for a tune-up, and resolves with an elusive,
+            unfinished ending. Even though this book has a start and an end, the
+            narrative of growing up extends beyond these pages. This anthology
+            represents a young, ever-evolving voice among billions, a voice that
+            is still learning that the fight for significance is not a
+            competition, but a demonstration of listening, rapport, and
+            compassion.
           </p>
         </Col>
       </Row>
