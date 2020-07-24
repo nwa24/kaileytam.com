f764c67995172649530d8e766dc5daa74a873ec4
nwa24 <nwa24@sfu.ca>
2020-07-23 17:32:52 -0700

    KW-14: Filtering markdown files with graphQL queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,20 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `blogs`,
+        path: `${__dirname}/src/content/blog-posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+const { createFilePath } = require("gatsby-source-filesystem")
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  let parentNode = getNode(node.parent)
+  if (node.internal.type === "MarkdownRemark") {
+    if (parentNode.sourceInstanceName === "blogs") {
+      let slug = createFilePath({ node, getNode })
+      slug = slug.replace(/\//g, "")
+      actions.createNodeField({ node, name: "slug", value: slug })
+    }
+  }
+}
