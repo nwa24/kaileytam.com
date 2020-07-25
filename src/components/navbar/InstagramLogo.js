f7b4aee0970ffc2611995a774f10ca9ac97bc118
nwa24 <nwa24@sfu.ca>
2020-07-25 14:36:05 -0700

    Updated the quality of the instagram logo

--- src/components/navbar/InstagramLogo.js
+++ src/components/navbar/InstagramLogo.js
@@ -1,5 +1,5 @@
 import React from "react"
-import InstagramIcon from "../../images/instagram-logo.png"
+import InstagramIcon from "../../images/instagram-logo2.png"
 
 const InstagramLogo = () => {
   return (
@@ -8,7 +8,11 @@ const InstagramLogo = () => {
       className="instagramLogo"
       style={{ paddingBottom: "5px" }}
     >
-      <img src={InstagramIcon} alt="logo" />
+      <img
+        style={{ width: "15px", height: "15px" }}
+        src={InstagramIcon}
+        alt="logo"
+      />
     </a>
   )
 }
